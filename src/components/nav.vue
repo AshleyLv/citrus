a60e52c9889a5ee99574d1700b0d04b044bb886d
吕一凡 <lvyifan@cnipr.com>
2018-11-26 15:31:46 +0800

    adding muse ui

---
+++ src/components/nav.vue
@@ -0,0 +1,26 @@
+<template>
+    <mu-appbar style="width: 100%;" color="primary" :textColor="navColor" :color="color">
+        <mu-button icon slot="left">
+            <mu-icon value="menu"></mu-icon>
+        </mu-button>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+
+
+        <mu-button flat slot="right">LOGIN</mu-button>
+    </mu-appbar>
+</template>
+<script>
+    export default {
+        name: 'nav',
+        data(){
+            return {
+                navColor:'#fff',
+                color:'#ccc'
+            }
+        }
+    }
+</script>
+<style lang="scss">
+
+</style>
